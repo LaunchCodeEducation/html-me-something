d671d22f85ce9e9ec83bf391a7c837b6af39af86
Chris Bay <chris@chrisbay.net>
2016-08-18 14:09:13 -0500

    Add build script for index.html

---
+++ submissions/build_page.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cat header.html > index.html
+
+for i in $( ls -d */ ); do
+    echo "          <li><a href=\""$i"\">@"${i%%/}"</a></li>" >> index.html
+done
+
+cat footer.html >> index.html
