a8a3e2cc5bf445d747ee6ae5e128ce96f3c44dfb
Chris Bay <chris@chrisbay.net>
2016-08-18 16:30:46 -0500

    Add styling to submission index

--- submissions/build_page.sh
+++ submissions/build_page.sh
@@ -3,7 +3,7 @@
 cat header.html > index.html
 
 for i in $( ls -d */ ); do
-    echo "          <li><a href=\""$i"\">@"${i%%/}"</a></li>" >> index.html
+    echo "              <li><a href=\""$i"\">@"${i%%/}"</a></li>" >> index.html
 done
 
 cat footer.html >> index.html
